42ae1ee7d787a8c95faac6f169a748384bc605c5
Lysander Mealy <lysander.mealy@gmail.com>
2022-12-24 16:07:20 -0500

    fix(cli.rs): Better code coverage.
    Add a test for argument parsing.

--- paxy/src/cli.rs
+++ paxy/src/cli.rs
@@ -1,12 +1,12 @@
 use clap::{Parser, Subcommand};
 
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, PartialEq, Eq)]
 pub struct Args {
     #[clap(subcommand)]
     pub command: Option<Command>,
 }
 
-#[derive(Subcommand, Debug)]
+#[derive(Subcommand, Debug, PartialEq, Eq)]
 pub enum Command {
     #[command(name = "search", about = "Search for a package")]
     Search { query: Vec<String> },
@@ -16,4 +16,22 @@ pub enum Command {
     Remove { packages: Vec<String> },
     #[command(name = "upgrade", about = "Upgrade package(s)")]
     Upgrade { packages: Vec<String> },
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn arg_parsing() {
+        let args = Args::parse_from(["paxy", "search", "foo"]);
+        assert_eq!(
+            args,
+            Args {
+                command: Some(Command::Search {
+                    query: vec!["foo".to_string()]
+                })
+            }
+        );
+    }
 }
