b1d0c70cd64d7d47032a1fb711b4602526146e56
shivanandvp <shivanandvp.oss@gmail.com>
2023-07-05 00:22:18 -0500

    docs: :art: Add favicon and logo on Rustdoc

--- paxy/src/main.rs
+++ paxy/src/main.rs
@@ -1,7 +1,9 @@
 #![warn(missing_docs)]
+#![doc(html_favicon_url = "https://raw.githubusercontent.com/PaxyHub/.github/main/paxy_logo.png")]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/PaxyHub/.github/main/paxy_logo.svg")]
 
 //! The main binary crate which contains the commandline interface used to
-//! interact with the program while it is running.
+//! interact with the paxy program while it is running.
 
 mod cli;
 mod config;
